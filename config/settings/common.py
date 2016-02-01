a4f4efee29ffa7bbf37a568df7326609b0eb02f0
tejasmehta <tejasmehta0@gmail.com>
2016-02-01 11:38:51 -0500

    first article

--- config/settings/common.py
+++ config/settings/common.py
@@ -48,6 +48,7 @@ THIRD_PARTY_APPS = (
 # Apps specific for this project go here.
 LOCAL_APPS = (
     'tejas.users',  # custom users app
+    'tejas.pages',
     # Your stuff: custom apps go here
 )
 
--- tejas/pages/views.py
+++ tejas/pages/views.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, unicode_literals
 
 from django.core.urlresolvers import reverse
 from django.http import Http404
+from django.shortcuts import render
+from django.template.loader import get_template
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
 
 from braces.views import LoginRequiredMixin
@@ -15,9 +17,9 @@ def projects(request, slug):
 	pass
 
 def writings(request, slug):
-	file_name = slug.replace('-', '_') + '.html'
+	file_name = (slug.replace('-', '_') + '.html').lower()
 	try:
 		t = get_template('writings/' + file_name)
 	except:
-		return Http404("Sorry no such page exists")
+		raise Http404("Sorry no such page exists")
 	return render(request, 'writings/' + file_name)
