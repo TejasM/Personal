264168fb0fb35e4b12e271e9d6a783f854b851ac
tejasmehta <tejasmehta0@gmail.com>
2016-02-08 12:17:03 -0500

    First project added

--- tejas/pages/views.py
+++ tejas/pages/views.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.core.urlresolvers import reverse
 from django.http import Http404
 from django.shortcuts import render
 from django.template.loader import get_template
-from django.views.generic import DetailView, ListView, RedirectView, UpdateView
-
-from braces.views import LoginRequiredMixin
 
 
 def learnings(request, slug):
-	pass
+	file_name = (slug.replace('-', '_') + '.html').lower()
+	try:
+		t = get_template('learnings/' + file_name)
+	except:
+		raise Http404("Sorry no such page exists")
+	return render(request, 'learnings/' + file_name)
 
 def projects(request, slug):
-	pass
+	file_name = (slug.replace('-', '_') + '.html').lower()
+	try:
+		t = get_template('projects/' + file_name)
+	except:
+		raise Http404("Sorry no such page exists")
+	return render(request, 'projects/' + file_name)
 
 def writings(request, slug):
 	file_name = (slug.replace('-', '_') + '.html').lower()
