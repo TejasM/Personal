fdaf26f9deca8e44565726d05e8c86e5acccdeb9
tejasmehta <tejasmehta0@gmail.com>
2016-02-14 10:39:12 -0500

    jtbd + filebrowser

--- config/settings/common.py
+++ config/settings/common.py
@@ -43,6 +43,8 @@ THIRD_PARTY_APPS = (
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'grappelli',
+    'filebrowser',
 )
 
 # Apps specific for this project go here.
